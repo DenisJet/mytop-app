a5015dcf267cd582b61ae8de6a60047b3aa9f503
Denis Kalkopf <deniskalkopf@gmail.com>
2023-12-16 11:21:30 +0500

    add Tag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag, Ptag } from '@/components';
+import { Button, Htag, Ptag, Tag } from '@/components';
 
 export default function Page(): JSX.Element {
   return (
@@ -13,6 +13,16 @@ export default function Page(): JSX.Element {
       <Ptag fontSize='large'>P tag large text</Ptag>
       <Ptag>P tag medium text</Ptag>
       <Ptag fontSize='small'>P tag small text</Ptag>
+      <Tag size='small' color='ghost'>
+        Tag
+      </Tag>
+      <Tag size='medium' color='red'>
+        Tag
+      </Tag>
+      <Tag size='medium' color='green'>
+        Tag
+      </Tag>
+      <Tag color='primary'>Tag</Tag>
     </>
   );
 }
