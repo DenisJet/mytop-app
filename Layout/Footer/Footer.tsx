9c40685043b91467fa85e2165139b7dfe446cc76
Denis Kalkopf <deniskalkopf@gmail.com>
2023-12-19 11:54:37 +0500

    layout layout

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer {...props} className={cn(className, styles.footer)}>
+      <div>OwlTop © 2022 - {format(new Date(), 'yyyy')} Все права защищены</div>
+      <a href='#' target='_blank'>
+        Пользовательское соглашение
+      </a>
+      <a href='#' target='_blank'>
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
