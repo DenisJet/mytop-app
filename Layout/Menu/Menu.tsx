668b43520462676c45d2819333a3b8b3188fe608
Denis Kalkopf <deniskalkopf@gmail.com>
2023-12-20 15:56:12 +0500

    add menu first level

--- Layout/Menu/Menu.tsx
+++ Layout/Menu/Menu.tsx
@@ -1,17 +1,48 @@
 import { AppContext } from '@/context/app.context';
+import { FirstLevelMenuItem } from '@/inteafaces/menu.interfaces';
 import { useContext } from 'react';
+import CoursesIcon from './icons/courses.svg';
+import ServicesIcon from './icons/services.svg';
+import BooksIcon from './icons/books.svg';
+import ProductsIcon from './icons/products.svg';
+import { TopLevelCategory } from '@/inteafaces/page.interface';
+import styles from './Menu.module.css';
+import cn from 'classnames';
+
+const firstLevelMenu: FirstLevelMenuItem[] = [
+  { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
+  { route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
+  { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
+  { route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
+];
 
 export const Menu = (): JSX.Element => {
   const { menu, setMenu, firstCategory } = useContext(AppContext);
 
-  return (
-    <div>
-      {' '}
-      <ul>
-        {menu.map((m) => (
-          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+  const buildFirstLevel = () => {
+    return (
+      <>
+        {firstLevelMenu.map((menu) => (
+          <div key={menu.route}>
+            <a href={`/${menu.route}`}>
+              <div
+                className={cn(styles.firstLevel, {
+                  [styles.firstLevelActive]: menu.id == firstCategory,
+                })}
+              >
+                {menu.icon}
+                <span>{menu.name}</span>
+              </div>
+            </a>
+          </div>
         ))}
-      </ul>
-    </div>
-  );
+      </>
+    );
+  };
+
+  const buildSecondLevel = () => {};
+
+  const buildThirdLevel = () => {};
+
+  return <div className={styles.menu}>{buildFirstLevel()}</div>;
 };
