c1b091d98e5785d695a7a2216468a5f94a7fa0eb
Denis Kalkopf <deniskalkopf@gmail.com>
2023-12-20 14:47:42 +0500

    add context

---
+++ context/app.context.tsx
@@ -0,0 +1,25 @@
+import { ReactNode, createContext, useState } from 'react';
+import { MenuItem } from '../inteafaces/menu.interfaces';
+import { TopLevelCategory } from '../inteafaces/page.interface';
+
+export interface IAppContext {
+  menu: MenuItem[];
+  firstCategory: TopLevelCategory;
+  setMenu?: (newMenu: MenuItem[]) => void;
+}
+
+export const AppContext = createContext<IAppContext>({ menu: [], firstCategory: TopLevelCategory.Courses });
+
+export const AppContextProvider = ({
+  menu,
+  firstCategory,
+  children,
+}: IAppContext & { children: ReactNode }): JSX.Element => {
+  const [menuState, setMenuState] = useState<MenuItem[]>(menu);
+
+  const setMenu = (newMenu: MenuItem[]) => {
+    setMenuState(newMenu);
+  };
+
+  return <AppContext.Provider value={{ menu: menuState, firstCategory, setMenu }}>{children}</AppContext.Provider>;
+};
