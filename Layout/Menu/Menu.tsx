8ae465e4540711a67cb9317ee18fd35277c0ef4e
Denis Kalkopf <deniskalkopf@gmail.com>
2023-12-21 11:40:07 +0500

    add Link

--- Layout/Menu/Menu.tsx
+++ Layout/Menu/Menu.tsx
@@ -8,6 +8,7 @@ import ProductsIcon from './icons/products.svg';
 import { TopLevelCategory } from '@/inteafaces/page.interface';
 import cn from 'classnames';
 import styles from './Menu.module.css';
+import Link from 'next/link';
 
 const firstLevelMenu: FirstLevelMenuItem[] = [
   { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
@@ -24,16 +25,18 @@ export const Menu = (): JSX.Element => {
       <>
         {firstLevelMenu.map((m) => (
           <div key={m.route}>
-            <a href={`/${m.route}`}>
-              <div
-                className={cn(styles.firstLevel, {
-                  [styles.firstLevelActive]: m.id == firstCategory,
-                })}
-              >
-                {m.icon}
-                <span>{m.name}</span>
-              </div>
-            </a>
+            <Link href={`/${m.route}`}>
+              <a>
+                <div
+                  className={cn(styles.firstLevel, {
+                    [styles.firstLevelActive]: m.id == firstCategory,
+                  })}
+                >
+                  {m.icon}
+                  <span>{m.name}</span>
+                </div>
+              </a>
+            </Link>
             {m.id == firstCategory && buildSecondLevel(m)}
           </div>
         ))}
@@ -62,15 +65,15 @@ export const Menu = (): JSX.Element => {
 
   const buildThirdLevel = (pages: PageItem[], route: string) => {
     return pages.map((p) => (
-      <a
-        key={p._id}
-        href={`/${route}/${p.alias}`}
-        className={cn(styles.thirdLevel, {
-          [styles.thirdLevelActive]: false,
-        })}
-      >
-        {p.category}
-      </a>
+      <link href={`/${route}/${p.alias}`} key={p._id}>
+        <a
+          className={cn(styles.thirdLevel, {
+            [styles.thirdLevelActive]: false,
+          })}
+        >
+          {p.category}
+        </a>
+      </link>
     ));
   };
 
