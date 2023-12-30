828120fec7d7241f1b13c0d0f3b3d34fc7baf8a5
Denis Kalkopf <deniskalkopf@gmail.com>
2023-12-30 21:56:13 +0500

    add keyboard accessibility of form

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,20 +1,34 @@
 import { RatingProps } from './Rating.props';
 import styles from './Rating.module.css';
 import cn from 'classnames';
-import { ForwardedRef, KeyboardEvent, forwardRef, useEffect, useState } from 'react';
+import { ForwardedRef, KeyboardEvent, forwardRef, useEffect, useRef, useState } from 'react';
 import StarIcon from './star.svg';
 import { error } from 'console';
 
 export const Rating = forwardRef(
   (
-    { isEditable = false, error, rating, setRating, ...props }: RatingProps,
+    { isEditable = false, error, rating, setRating, tabIndex, ...props }: RatingProps,
     ref: ForwardedRef<HTMLDivElement>
   ): JSX.Element => {
     const [ratingArray, setRattingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
+    const ratingArrayRef = useRef<(HTMLSpanElement | null)[]>([]);
 
     useEffect(() => {
       constructRating(rating);
-    }, [rating]);
+    }, [rating, tabIndex]);
+
+    const computeFocus = (r: number, i: number): number => {
+      if (!isEditable) {
+        return -1;
+      }
+      if (!rating && i == 0) {
+        return tabIndex ?? 0;
+      }
+      if (r == i + 1) {
+        return tabIndex ?? 0;
+      }
+      return -1;
+    };
 
     const constructRating = (currentRating: number) => {
       const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
@@ -28,11 +42,11 @@ export const Rating = forwardRef(
             onMouseEnter={() => changeDisplay(i + 1)}
             onMouseLeave={() => changeDisplay(rating)}
             onClick={() => onclick(i + 1)}
+            tabIndex={computeFocus(rating, i)}
+            onKeyDown={handleKey}
+            ref={(r) => ratingArrayRef.current?.push(r)}
           >
-            <StarIcon
-              tabIndex={isEditable ? 0 : -1}
-              onKeyDown={(e: KeyboardEvent<SVGAElement>) => isEditable && handleSpace(i + 1, e)}
-            />
+            <StarIcon />
           </span>
         );
       });
@@ -53,11 +67,24 @@ export const Rating = forwardRef(
       setRating(i);
     };
 
-    const handleSpace = (i: number, e: KeyboardEvent) => {
-      if (e.code != 'Space' || !setRating) {
+    const handleKey = (e: KeyboardEvent) => {
+      if (!isEditable || !setRating) {
         return;
       }
-      setRating(i);
+      if (e.code == 'ArrowRight' || e.code == 'ArrowUp') {
+        if (!rating) {
+          setRating(1);
+        } else {
+          e.preventDefault();
+          setRating(rating < 5 ? rating + 1 : 5);
+        }
+        ratingArrayRef.current[rating]?.focus();
+      }
+      if (e.code == 'ArrowLeft' || e.code == 'ArrowDown') {
+        e.preventDefault();
+        setRating(rating > 1 ? rating - 1 : 1);
+        ratingArrayRef.current[rating - 2]?.focus();
+      }
     };
 
     return (
--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -10,7 +10,7 @@ import { API } from '@/helpers/api';
 import axios from 'axios';
 import { useState } from 'react';
 
-export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
+export const ReviewForm = ({ productId, isOpened, className, ...props }: ReviewFormProps): JSX.Element => {
   const {
     register,
     control,
@@ -42,12 +42,14 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
           {...register('name', { required: { value: true, message: 'Введите заголовок' } })}
           placeholder='Имя'
           error={errors.name}
+          tabIndex={isOpened ? 0 : -1}
         />
         <Input
           {...register('title', { required: { value: true, message: 'Введите заголовок' } })}
           placeholder='Заголовок отзыва'
           className={styles.title}
           error={errors.title}
+          tabIndex={isOpened ? 0 : -1}
         />
         <div className={styles.rating}>
           <span>Оценка:</span>
@@ -62,6 +64,7 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
                 ref={field.ref}
                 setRating={field.onChange}
                 error={errors.rating}
+                tabIndex={isOpened ? 0 : -1}
               />
             )}
           />
@@ -71,9 +74,12 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
           placeholder='Текст отзыва'
           className={styles.description}
           error={errors.description}
+          tabIndex={isOpened ? 0 : -1}
         />
         <div className={styles.submit}>
-          <Button appearance='primary'>Отправить</Button>
+          <Button appearance='primary' tabIndex={isOpened ? 0 : -1}>
+            Отправить
+          </Button>
           <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
         </div>
       </div>
