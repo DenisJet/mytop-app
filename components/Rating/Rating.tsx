59f5c15a128b8e592fbb25a19bd1ae0bf8186a58
Denis Kalkopf <deniskalkopf@gmail.com>
2024-01-03 15:10:19 +0500

    add forms accessibility

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -45,6 +45,12 @@ export const Rating = forwardRef(
             tabIndex={computeFocus(rating, i)}
             onKeyDown={handleKey}
             ref={(r) => ratingArrayRef.current?.push(r)}
+            role={isEditable ? 'slider' : ''}
+            aria-label={isEditable ? 'Укажите рейтинг' : 'рейтинг' + rating}
+            aria-invalid={error ? true : false}
+            aria-aria-valuenow={rating}
+            aria-valuemax={5}
+            aria-valuemin={1}
           >
             <StarIcon />
           </span>
@@ -98,7 +104,11 @@ export const Rating = forwardRef(
         {ratingArray.map((rating, index) => (
           <span key={index}>{rating}</span>
         ))}
-        {error && <span className={styles.errorMessage}>{error.message}</span>}
+        {error && (
+          <span role='alert' className={styles.errorMessage}>
+            {error.message}
+          </span>
+        )}
       </div>
     );
   }
--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -17,6 +17,7 @@ export const ReviewForm = ({ productId, isOpened, className, ...props }: ReviewF
     handleSubmit,
     formState: { errors },
     reset,
+    clearErrors,
   } = useForm<IReviewForm>();
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
   const [isError, setIsError] = useState<string>();
@@ -43,6 +44,7 @@ export const ReviewForm = ({ productId, isOpened, className, ...props }: ReviewF
           placeholder='Имя'
           error={errors.name}
           tabIndex={isOpened ? 0 : -1}
+          aria-invalid={errors.name ? true : false}
         />
         <Input
           {...register('title', { required: { value: true, message: 'Введите заголовок' } })}
@@ -50,6 +52,7 @@ export const ReviewForm = ({ productId, isOpened, className, ...props }: ReviewF
           className={styles.title}
           error={errors.title}
           tabIndex={isOpened ? 0 : -1}
+          aria-invalid={errors.title ? true : false}
         />
         <div className={styles.rating}>
           <span>Оценка:</span>
@@ -75,9 +78,11 @@ export const ReviewForm = ({ productId, isOpened, className, ...props }: ReviewF
           className={styles.description}
           error={errors.description}
           tabIndex={isOpened ? 0 : -1}
+          aria-label='Текст отзыва'
+          aria-invalid={errors.description ? true : false}
         />
         <div className={styles.submit}>
-          <Button appearance='primary' tabIndex={isOpened ? 0 : -1}>
+          <Button appearance='primary' tabIndex={isOpened ? 0 : -1} onClick={() => clearErrors()}>
             Отправить
           </Button>
           <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
