2fdfee0f89bdc8f333d19ae09d437bf9e6f35357
Denis Kalkopf <deniskalkopf@gmail.com>
2024-01-06 20:18:35 +0500

    add page 404

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import { withLayout } from '@/Layout/Layout';
+import { Htag } from '@/components';
+
+function Error404(): JSX.Element {
+  return (
+    <>
+      <Htag tag='h1'>Ошибка 404</Htag>
+    </>
+  );
+}
+
+export default withLayout(Error404);
