791d9c2ee9767396c0eae3ab72f5106a90f0af8e
Denis Kalkopf <deniskalkopf@gmail.com>
2024-01-06 12:12:42 +0500

    add ym

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 
 export default function App({ Component, pageProps, router }: AppProps): JSX.Element {
+  router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+      ym('hit', url);
+    }
+  });
+
   return (
     <>
       <Head>
         <title>MyTop</title>
-        <link rel='icon' href='/public/favicon.ico' />
+        <link rel='icon' href='/favicon.ico' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
           href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
@@ -15,6 +23,7 @@ export default function App({ Component, pageProps, router }: AppProps): JSX.Ele
         />
         <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
         <meta property='og:local' content='ru_Ru' />
+        <YMInitializer accounts={[]} options={{ webvisor: true, defer: true }} version='2' />
       </Head>
       <Component {...pageProps} />
     </>
